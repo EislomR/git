d6594ffa3295df7e926a3ada3fc62830f3166710
Islom <eislomr0329@gmail.com>
2022-03-10 16:42:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+"use strict"
+// const byName = "Islom";
+// console.log(byName);
+
+// let byName = "Islom";
+// byName = "Kitob";
+// console.log(byName);
+
+// const obj={
+//     a:10,
+// };
+// obj.a=20;
+// console.log(obj);
+
+// x=15;
+// console.log(x);
+
+// let = firstName;
+// console.log(firstName);
+
+// const person = {
+//     firstName: "Islom",
+//     lastName: "Ergashev",
+//     age: 16,
+//     job: "Webdeveloper",
+//     isMarried: false,
+// };
+// console.log(person);
+
+// console.log(5+5);
+// console.log(5-2);
+// console.log(5*2);
+// console.log(5/1);
+
+// const x=10, y=5;
+// console.log(x,y);
+
+// console.log("Hello World");
+
+// const num = 4 + "radio" ;
+// console.log(typeof num);
+
+// const num = "radio" +4;
+// console.log(typeof num);
+
+// let inc = 99;
+// console.log(++inc);
+
+// let dicr = 99;
+// console.log(--dicr);
+
+// console.log(2*3==="6");
+// console.log(2*3=="6");
+
+// let a = 2;
+// let b = "4";
+// console.log(a+b);
+
+// console.log(10+8);
+// console.log(100-1);
+// console.log(50*60);
+// console.log(100000/50);
+
+// const x=45, y=50;
+// console.log(x,y);
+
+// console.log("Ergashev Islom");
+
+// const num = 10 + "Islom" ;
+// console.log(typeof num);
+
+// const arr = ["red", "black", "yellow"];
+// console.log(arr[1]);
+// dasturlashda sanash 0 dan boshlanadi
+
+// alert
+// prompt
+// confirm
+
+// alert("Salom");
+// prompt("Ismingizni kiriting")
+// confirm("Sizning yoshingiz 16dami")
+
+// const userAge = confirm("siz 16 yoshdamisiz?");
+// console.log(userAge);
+
+// const userName = prompt("ismingiz nima?");
+// console.log(userName);
+
+// const answers = [];
+// answers[0] = prompt("ismingiz nima"); 
+// answers[1] = prompt("familyangiz nima");
+// answers[2] = prompt("yoshingiz nechchida");
+
+// console.log(answers);
+
